1727a7280780b272294d5ca3ca2658e5b2c0f95f
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2023-12-28 09:32:08 +0530

    finally its' working again. lets push before breaking

--- morse.ino
+++ morse.ino
@@ -1,23 +1,22 @@
-// TODOs that I'll probably forget.
-// 1. Training mode for training the dot-dash times.
-
 #include <LiquidCrystal_I2C.h>
 #include <stdarg.h>
 
 // I2C Address of the LCD Display module:
 #define LCD_DISPLAY_I2C_ADDR 0x27
-#define LCD_LINE_LENGTH 16
 #define CHARSET_LENGTH 36
 #define UNIT_DELAY 200  // time in ms that is considered as one unit.
 #define MAX_INPUT_BUFFER_LENGTH 5
 const char INVALID_CHARACTER = '?';
+
+
+const int LCD_LINE_LENGTH = 16;
 
 const int BUZZER_PIN = 7;
 const int BUTTON_PIN = 2;
 const int LED_PIN = LED_BUILTIN;  // for now
 LiquidCrystal_I2C display(LCD_DISPLAY_I2C_ADDR, LCD_LINE_LENGTH, 2);
 
-bool wasHolding = false;
+bool isHolding = false;
 unsigned long holdStart;
 unsigned long releaseStart;
 char position = 0;
@@ -72,7 +71,7 @@ const char* MORSE_CODES[36][2] = {
 char getCharacter(char morse[]) {
   for (int i = 0; i < CHARSET_LENGTH; i++)
     if (strcmp(MORSE_CODES[i][0], morse) == 0) return MORSE_CODES[i][1][0];
-  return INVALID_CHARACTER;  // Negative one if not found.
+  return INVALID_CHARACTER;
 }
 
 // Fill an character array with the given character.
@@ -84,9 +83,39 @@ char LINE_BUFFER[LCD_LINE_LENGTH];
 char INPUT_BUFFER[MAX_INPUT_BUFFER_LENGTH] = { ' ', ' ', ' ', ' ', ' ' };
 char OUTPUT_BUFFER[LCD_LINE_LENGTH];
 
+int add(int x, int y) {
+  return x + y;
+}
+int subtract(int x, int y) {
+  return x - y;
+}
+bool lessThan(int x, int y) {
+  return x < y;
+}
+bool greaterThan(int x, int y) {
+  return x > y;
+}
+
+void printCharByChar(char* str, int delayMs, int line, int indent, bool reverse = false) {
+  int start = reverse ? strlen(str) - 1 : 0;
+  auto limitFn = reverse ? greaterThan : lessThan;
+  int limit = reverse ? -1 : strlen(str);
+  auto updateFn = reverse ? subtract : add;
+  for (int i = start; limitFn(i, limit); i = updateFn(i, 1)) {
+    display.setCursor(indent + i, line);
+    display.print(str[i]);
+    delay(delayMs);
+  }
+}
+
+uint8_t envelope1[8] = { 0x1f, 0x18, 0x14, 0x12, 0x11, 0x10, 0x10, 0x1f };
+uint8_t envelope2[8] = { 0x1f, 0x03, 0x05, 0x09, 0x11, 0x01, 0x01, 0x1f };
+
+unsigned long lastEvent = millis();
+
 void setup() {
   // for debugging purposes:
-  Serial.begin(9600);  
+  Serial.begin(9600);
 
   // Setup I/O pins
   pinMode(BUZZER_PIN, OUTPUT);
@@ -96,60 +125,119 @@ void setup() {
   // Setup LCD I2C 16x2 display
   display.init();
   display.backlight();
+  display.createChar(0, envelope1);
+  display.createChar(1, envelope2);
 
   // Set initial states for pins and modules.
   digitalWrite(BUZZER_PIN, LOW);
   digitalWrite(LED_PIN, LOW);
   digitalWrite(BUTTON_PIN, HIGH);
   display.clear();
+
+  // Mini splash screen
+  printCharByChar("Morse Code", 50, 0, 3);
+  printCharByChar("Translator", 50, 1, 3, true);
+  delay(1500);
+  display.clear();
+
+  printCharByChar("Group 8B", 50, 0, 4);
+  printCharByChar("58  60  61  62", 50, 1, 1, true);
+  delay(1000);
+  display.clear();
+
+  lastEvent = millis();
+}
+
+void printEnvelopeChar() {
+  display.write(0);
+  display.write(1);
 }
 
 void loop() {
   int buttonState = digitalRead(BUTTON_PIN);
-  // Serial.println(buttonState);
 
-  if (buttonState == LOW && !wasHolding) {        // started holding
-    wasHolding = true, holdStart = millis();      // just record the values.
-  } else if (buttonState == LOW && wasHolding) {  // still holding
-    // TODO: Move the state management from using the release time to hold time.
-  } else if (buttonState == HIGH && wasHolding) {  // stopped holding
-    // Depending on the hold time, differentiate dots and dashes and push them to the input buffer.
-    wasHolding = false, releaseStart = millis();
+  // if (Serial.available()) {
+  //   String input = Serial.readString();
+  //   input.trim();
+  //   if (input[0] != 0) {
+  //     display.clear();
+  //     display.backlight();
+  //     display.home();
+  //     printEnvelopeChar();
+  //     display.print(" New message!");
+  //     delay(3000);
+  //     display.clear();
+
+  //     char filtered[input.length()];
+  //     for (int i = 0; i < input.length(); i++) {
+  //       char character = input.charAt(i);
+  //       filtered[i] = (i >= 65 && i <= 90) || (i >= 48 && i <= 57)
+  //         ? character
+  //         : i >= 97 && i <= 122
+  //         ? character - 22
+  //         : INVALID_CHARACTER;
+  //     }
+
+  //     display.home();
+  //     printEnvelopeChar();
+  //     display.setCursor(10, 0);
+
+  //     char buffer[11];
+  //     for (int i = 0; i < strlen(filtered); i++) {
+  //       char c = filtered[i];
+
+  //     }
+
+  //     display.clear();
+  //     lastEvent = millis();
+  //   }
+  // }
+
+  if (buttonState == LOW && !isHolding) {
+    display.backlight();
+    isHolding = true, holdStart = millis(), lastEvent = millis();
+    digitalWrite(BUZZER_PIN, HIGH);
+  } else if (buttonState == LOW && isHolding) {
+    releaseStart = millis(), lastEvent = millis();
     unsigned long holdTime = releaseStart - holdStart;
-
-    INPUT_BUFFER[position] = holdTime < 2 * UNIT_DELAY ? '.' : '-';
-    partUpdated = false, letterUpdated = false, wordUpdated = false;  // condaminate them all.
-
-    println(0, INPUT_BUFFER);                       // print the updated input buffer.
-  } else if (buttonState == HIGH && !wasHolding) {  // doing nothing
+    if (holdTime > 3000) {  // hold for 3 seconds to clear the display.
+      fillChar(INPUT_BUFFER, ' ', MAX_INPUT_BUFFER_LENGTH);
+      fillChar(OUTPUT_BUFFER, '\0', LCD_LINE_LENGTH);
+      digitalWrite(BUZZER_PIN, LOW);
+      display.clear();
+      partUpdated = true, letterUpdated = true, wordUpdated = true;
+    } else {
+      INPUT_BUFFER[position] = holdTime < 2 * UNIT_DELAY ? '.' : '-';
+      partUpdated = false, letterUpdated = false, wordUpdated = false;
+      printInputBuffer();
+    }
+  } else if (buttonState == HIGH && isHolding) {
+    isHolding = false, releaseStart = millis(), lastEvent = millis();
+    digitalWrite(BUZZER_PIN, LOW);
+  } else if (buttonState == HIGH && !isHolding) {
     unsigned long releaseTime = millis() - releaseStart;
-
-    if (releaseTime < 3 * UNIT_DELAY && !partUpdated) {
-      // TIME DELAY 1: MORSE CODE PART (DOT/DASH)
-      // Just update the position.
+    if (millis() - lastEvent > 10000) {
+      display.noBacklight();
+    } else if (releaseTime < 3 * UNIT_DELAY && !partUpdated) {
       position = position + 1 == 5 ? 0 : position + 1;
+      if (position == 0) {
+        append(OUTPUT_BUFFER, getCharacter(INPUT_BUFFER));
+        printInputBuffer(), printOutputBuffer();
+        letterUpdated = true;
+        fillChar(INPUT_BUFFER, ' ', MAX_INPUT_BUFFER_LENGTH);
+      }
       partUpdated = true;
     } else if (releaseTime >= 3 * UNIT_DELAY && releaseTime < 7 * UNIT_DELAY && !letterUpdated) {
-      append(OUTPUT_BUFFER, getCharacter(INPUT_BUFFER));  // append the decoded character to the output
-
-      println(0, INPUT_BUFFER);
-      println(1, OUTPUT_BUFFER);
-
-      letterUpdated = true, position = 0;                    // the letter is now updated.
-      fillChar(INPUT_BUFFER, ' ', MAX_INPUT_BUFFER_LENGTH);  // clear and prepare input buffer for the next letter.
+      append(OUTPUT_BUFFER, getCharacter(INPUT_BUFFER));
+      printInputBuffer(), printOutputBuffer();
+      letterUpdated = true, position = 0;
+      fillChar(INPUT_BUFFER, ' ', MAX_INPUT_BUFFER_LENGTH);
     } else if (releaseTime >= 7 * UNIT_DELAY && !wordUpdated) {
-      // TIME DELAY 3: SEPARATING WORDS
-      // NOTE: The check can be true for the very first morse letter input, because the releaseTime would be a
-      // higher value since the releaseStart is set to ZERO and the word wouldn't have been updated yet.
-
-      // To prevent adding spaces unnecessarily, only append the <SPACE> if
-      // - there have been some content added before it. (handles the VERY first input event)
-      // - the previous character isn't an space. we don't want multiple spaces ofc.
       if (strlen(OUTPUT_BUFFER) > 0 && OUTPUT_BUFFER[strlen(OUTPUT_BUFFER) - 1] != ' ') {
         append(OUTPUT_BUFFER, ' ');
       }
-      wordUpdated = true, position = 0;                      // the word is updated now.
-      fillChar(INPUT_BUFFER, ' ', MAX_INPUT_BUFFER_LENGTH);  // clear and prepare input buffer for the next letter.
+      wordUpdated = true, position = 0;
+      fillChar(INPUT_BUFFER, ' ', MAX_INPUT_BUFFER_LENGTH);
     }
   }
 }
@@ -161,6 +249,16 @@ char* append(char content[], char supplement) {
       content[i] = i + 1 != LCD_LINE_LENGTH ? content[i + 1] : supplement;
   } else content[strlen(content)] = supplement;
   return content;
+}
+
+void printInputBuffer() {
+  display.setCursor(0, 0);
+  display.print(INPUT_BUFFER);
+}
+
+void printOutputBuffer() {
+  display.setCursor(0, 1);
+  display.print(OUTPUT_BUFFER);
 }
 
 // Convenience function for printing a line to the LCD
